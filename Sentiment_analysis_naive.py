ea8e591024b1f6fde0eab29368ca72cfa378fa19
Siddharth Patra <siddp278@gmail.com>
2021-01-27 17:27:57 +0530

    Update Sentiment_analysis_naive.py

--- Sentiment_analysis_naive.py
+++ Sentiment_analysis_naive.py
@@ -1,4 +1,4 @@
-import pdb
+import pdb, pickle
 from nltk.corpus import stopwords, twitter_samples
 import numpy as np
 import pandas as pd
@@ -36,7 +36,6 @@ def lookup(freqs, word, label):
         return p
 
 
-# get the sets of positive and negative tweets
 all_positive_tweets = twitter_samples.strings('positive_tweets.json')
 all_negative_tweets = twitter_samples.strings('negative_tweets.json')
 
@@ -49,30 +48,8 @@ train_neg = all_negative_tweets[:4000]
 train_x = train_pos + train_neg
 test_x = test_pos + test_neg
 
-# avoid assumptions about the length of all_positive_tweets
 train_y = np.append(np.ones(len(train_pos)), np.zeros(len(train_neg)))
 test_y = np.append(np.ones(len(test_pos)), np.zeros(len(test_neg)))
-
-
-def process_tweet(tweet):
-    stemmer = nltk.PorterStemmer()
-    stopwords_english = stopwords.words('english')
-    tweet = re.sub(r'\$\w*', '', tweet)
-    tweet = re.sub(r'^RT[\s]+', '', tweet)
-    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
-    tweet = re.sub(r'#', '', tweet)
-    tokenizer = nltk.TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
-    tweet_tokens = tokenizer.tokenize(tweet)
-
-    tweets_clean = []
-    for word in tweet_tokens:
-        if (word not in stopwords_english and
-                word not in string.punctuation):
-            stem_word = stemmer.stem(word)  # stemming word
-            tweets_clean.append(stem_word)
-
-    return tweets_clean
-
 
 custom_tweet = "RT @Twitter @chapagain Hello There! Have a great day. :) #good #morning http://chapagain.com.np"
 
@@ -80,29 +57,23 @@ custom_tweet = "RT @Twitter @chapagain Hello There! Have a great day. :) #good #
 print(process_tweet(custom_tweet))
 
 def count_tweets(result, tweets, ys):
-    '''
+    """
     Input:
         result: a dictionary that will be used to map each pair to its frequency
         tweets: a list of tweets
         ys: a list corresponding to the sentiment of each tweet (either 0 or 1)
     Output:
         result: a dictionary mapping each pair to its frequency
-    '''
-
-    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+    """
     for y, tweet in zip(ys, tweets):
         for word in process_tweet(tweet):
-            # define the key, which is the word and label tuple
             pair = (word, y)
 
             # if the key exists in the dictionary, increment the count
             if pair in result:
                 result[pair] += 1
-
-            # else, if the key is new, add it to the dictionary and set the count to 1
             else:
                 result[pair] = 1
-    ### END CODE HERE ###
 
     return result
 
@@ -114,8 +85,9 @@ count_tweets(result, tweets, ys)
 
 freqs = count_tweets({}, train_x, train_y)
 
+
 def train_naive_bayes(freqs, train_x, train_y):
-    '''
+    """
     Input:
         freqs: dictionary from (word, label) to how often the word appears
         train_x: a list of tweets
@@ -123,48 +95,31 @@ def train_naive_bayes(freqs, train_x, train_y):
     Output:
         logprior: the log prior. (equation 3 above)
         loglikelihood: the log likelihood of you Naive bayes equation. (equation 6 above)
-    '''
+    """
     loglikelihood = {}
     logprior = 0
-
-    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
 
     # calculate V, the number of unique words in the vocabulary
     vocab = set([pair[0] for pair in freqs.keys()])
     V = len(vocab)
     # print(V)
-
-    # calculate N_pos and N_neg
     N_pos = N_neg = 0
     for pair in freqs.keys():
-        # if the label is positive (greater than zero)
         if pair[1] > 0:
-
-            # Increment the number of positive words by the count for this (word, label) pair
             N_pos += freqs[pair]
-
-        # else, the label is negative
         else:
-
-            # increment the number of negative words by the count for this (word,label) pair
             N_neg += freqs[pair]
 
-    # print(N_pos, N_neg)
-    # Calculate D, the number of documents
     D = len(train_x)
 
-    # Calculate D_pos, the number of positive documents (*hint: use sum(<np_array>))
+    # Calculate D_pos, the number of positive documents.
     D_pos = np.sum(train_y)
 
-    # Calculate D_neg, the number of negative documents (*hint: compute using D and D_pos)
+    # Calculate D_neg, the number of negative documents.
     D_neg = D - D_pos
 
-    # Calculate logprior
     logprior = np.log(D_pos) - np.log(D_neg)
-
-    # For each word in the vocabulary...
     for word in vocab:
-        # get the positive and negative frequency of the word
         # print(word)
         # print(lookup(freqs, word, 1))
         freq_pos = lookup(freqs, word, 1)
@@ -178,20 +133,28 @@ def train_naive_bayes(freqs, train_x, train_y):
         b =  p_w_pos/ p_w_neg
         loglikelihood[word] =  np.log(b)
 
-    ### END CODE HERE ###
-
     return logprior, loglikelihood
 
 
-
-# You do not have to input any code in this cell, but it is relevant to grading, so please do not change anything
 logprior, loglikelihood = train_naive_bayes(freqs, train_x, train_y)
 print(logprior)
 print(len(loglikelihood))
 
+# get logprior, loglikelihood ka pickle files needed.
+"""
+save_documents = open(r"Doc_logptiot.pickle", "wb")
+pickle.dump(logprior, save_documents)
+save_documents.close()
+
+save_documents1 = open(r"Doc_loglikelihood.pickle", "wb")
+pickle.dump(loglikelihood, save_documents1)
+save_documents1.close()
+"""
+
+
 
 def naive_bayes_predict(tweet, logprior, loglikelihood):
-    '''
+    """
     Input:
         tweet: a string
         logprior: a number
@@ -199,30 +162,20 @@ def naive_bayes_predict(tweet, logprior, loglikelihood):
     Output:
         p: the sum of all the logliklihoods of each word in the tweet (if found in the dictionary) + logprior (a number)
 
-    '''
-    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
-    # process the tweet to get a list of words
+    """
     word_l = process_tweet(tweet)
-
-    # initialize probability to zero
     p = 0
-
-    # add the logprior
     p += logprior
 
     for word in word_l:
 
         # check if the word exists in the loglikelihood dictionary
         if word in loglikelihood:
-            # add the log likelihood of that word to the probability
             p += loglikelihood.get(word)
-
-    ### END CODE HERE ###
 
     return p
 
 
-# Experiment with your own tweet.
 my_tweet = 'She smiled.'
 p = naive_bayes_predict(my_tweet, logprior, loglikelihood)
 print('The expected output is', p)
@@ -237,29 +190,19 @@ def test_naive_bayes(test_x, test_y, logprior, loglikelihood):
     Output:
         accuracy: (# of tweets classified correctly)/(total # of tweets)
     """
-    accuracy = 0  # return this properly
-
-    ### START CODE HERE (REPLACE INSTANCES OF 'None' with your code) ###
+    accuracy = 0
     y_hats = []
     for tweet in test_x:
-        # if the prediction is > 0
         if naive_bayes_predict(tweet, logprior, loglikelihood) > 0:
-            # the predicted class is 1
             y_hat_i = 1
         else:
-            # otherwise the predicted class is 0
             y_hat_i = 0
 
-        # append the predicted class to the list y_hats
         y_hats.append(y_hat_i)
 
     # error is the average of the absolute values of the differences between y_hats and test_y
     error = (y_hats != np.squeeze(test_y)).sum() / len(test_x)
-
-    # Accuracy is 1 minus the error
     accuracy = float(1 - error)
-
-    ### END CODE HERE ###
 
     return accuracy
 
@@ -270,5 +213,7 @@ print("Naive Bayes accuracy = %0.4f" %
 
 
 # Feel free to check the sentiment of your own tweet below
+# Note here that a positive value shows positive sentiment and its magnitude tells how much positive it is, similarly
+# for the negative tweets.
 my_tweet = 'you are bad :('
-naive_bayes_predict(my_tweet, logprior, loglikelihood)
+print(naive_bayes_predict(my_tweet, logprior, loglikelihood))
